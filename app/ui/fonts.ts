14c7cc754661f05185a17152de49749880d6a4c0
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-26 21:47:36 +0800

    feat(ui/fonts): added red_rose & marcellus_sc fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,23 @@
-import { Inter } from "next/font/google";
+import { Inter, Marcellus_SC, Red_Rose } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
-export { inter };
+const red_rose = Red_Rose({
+  subsets: ["latin"],
+  weight: ["300", "500", "700"],
+  variable: "--font-red-rose",
+  display: "swap",
+  preload: true,
+});
+
+const marcellus_sc = Marcellus_SC({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-marcellus-sc",
+  display: "swap",
+  preload: true,
+});
+
+export { inter, red_rose, marcellus_sc };

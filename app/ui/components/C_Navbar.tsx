c2afee2431a370e7382d5d19b840877e34d37230
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-29 13:11:27 +0800

    feat(ui/comps/): added link comps to redirect in auth page

--- app/ui/components/C_Navbar.tsx
+++ app/ui/components/C_Navbar.tsx
@@ -2,21 +2,25 @@
 
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 
 export default function C_Navbar({
   children,
-}: Readonly<{ children: ReactNode }>) {
+  className,
+}: Readonly<{ children: ReactNode; className?: string }>) {
   const pathname = usePathname();
 
   const atAuth = pathname.includes("/auth");
 
   return (
     <nav
-      className={clsx("fixed top-0 left-0 w-full py-6 px-8", {
-        "-translate-y-full": atAuth,
-        "translate-y-0": !atAuth,
-      })}
+      className={clsx(
+        `fixed top-0 left-0 w-full py-4 px-8 flex items-center justify-between ${className}`,
+        {
+          "-translate-y-full": atAuth,
+          "translate-y-0": !atAuth,
+        }
+      )}
     >
       {children}
     </nav>
--- app/ui/components/Navbar.tsx
+++ app/ui/components/Navbar.tsx
@@ -1,11 +1,15 @@
+import AuthTogglers from "@/app/auth/_components/AuthTogglers";
 import AppLinks from "./AppLinks";
 import C_Navbar from "./C_Navbar";
 
 export default function Navbar() {
   return (
-    <C_Navbar>
-      <ul className="flex items-center gap-3">
+    <C_Navbar className="children:flex children:items-center children:gap-3">
+      <ul>
         <AppLinks />
+      </ul>
+      <ul>
+        <AuthTogglers />
       </ul>
     </C_Navbar>
   );

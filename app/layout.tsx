a16a0fe5eb831a69d23a5a711a9fc70f3d7f107a
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-29 13:02:03 +0800

    feat(layout): utilized imported next/fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/ui/styles/globals.css";
-import { inter, marcellus_sc, red_rose } from "./ui/fonts";
+import { inter, lato, poppins } from "./ui/fonts";
 import Navbar from "./ui/components/Navbar";
 
 export const metadata: Metadata = {
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} ${red_rose.variable} ${marcellus_sc.variable}`}
+        className={`${inter.className} ${poppins.variable} ${lato.variable}`}
       >
         <Navbar />
         {children}

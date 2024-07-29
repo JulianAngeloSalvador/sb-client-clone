45a25f21c9ff7832a6211b78b8656063a3d746f6
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-29 12:59:30 +0800

    feat(lib/utils): added basic utility function for capitalizing each wordzz

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -2,4 +2,11 @@ const capitalize = (text: string) => {
   return `${text.charAt(0).toUpperCase()}${text.slice(1)}`;
 };
 
-export { capitalize };
+const capitalizeEach = (texts: string) => {
+  return texts
+    .split(" ")
+    .map((text) => capitalize(text))
+    .join(" ");
+};
+
+export { capitalize, capitalizeEach };

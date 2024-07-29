eafbb746857b3893d954e8e76cb2289c16868052
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-07-29 12:54:26 +0800

    feat(ui/fonts): transitioned to lato & poppins font-fam

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,23 +1,23 @@
-import { Inter, Marcellus_SC, Red_Rose } from "next/font/google";
+import { Inter, Lato, Poppins } from "next/font/google";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
-const red_rose = Red_Rose({
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "700", "900"],
+  variable: "--font-lato",
+  display: "swap",
+  preload: true,
+});
+
+const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "500", "700"],
-  variable: "--font-red-rose",
+  variable: "--font-poppins",
   display: "swap",
   preload: true,
 });
 
-const marcellus_sc = Marcellus_SC({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-marcellus-sc",
-  display: "swap",
-  preload: true,
-});
-
-export { inter, red_rose, marcellus_sc };
+export { inter, lato, poppins };
